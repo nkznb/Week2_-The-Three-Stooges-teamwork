09f5b88d9b4169b21d04ed82a88ba9faf3eeedd7
nkznb <nikai080421@gmail.com>
2026-09-14 10:29:45 +0800

    add score grading program 3.py

---
+++ 3.py
@@ -0,0 +1,24 @@
+def the_ui():
+    print("====================")
+    print("SCORE GRADING SYSTEM")
+    print("====================")
+
+# 函数改名，接收分数作为参数
+def get_grade(score_num):
+    if score_num >= 90:
+        print("Your grade is A")
+    elif score_num >= 80:
+        print("Your grade is B")
+    elif score_num >= 70:
+        print("Your grade is C")
+    elif score_num >= 60:
+        print("Your grade is D")
+    else:
+        print("Your grade is F")
+
+def main():
+    the_ui()
+    score = int(input("Please enter your score: "))
+    get_grade(score)
+
+main()
